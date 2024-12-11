7026b07dc0a9b00b28e0b4cfa83f94a10aa7f29b
GryphoDev <wmauroux@icloud.com>
2024-12-11 17:56:08 +0100

    fix: error page spacing

--- src/page/error/error.tsx
+++ src/page/error/error.tsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
+import { useIsLargeScreen } from "../../utils/hook/isLargeScreen.tsx";
 import styles from "./error.module.scss";
 
 export function Error() {
+  const isLargeScreen = useIsLargeScreen();
   return (
     <main className="mainContainer">
       <div className={styles.error}>
         <h1 className={styles.error__code}>404</h1>
         <p className={styles.error__message}>
-          Oups! La page que vous demandez n'existe pas.
+          {isLargeScreen ? (
+            "Oups! La page que vous demandez n'existe pas."
+          ) : (
+            <>
+              Oups! La page que <br /> vous demandez n'existe pas.
+            </>
+          )}
         </p>
         <Link className={styles.error__linkToHome} to="/">
           Retourner sur la page d’accueil
